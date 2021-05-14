be632d3af982759ea9807ce286c7e6dbb1163a9e
Adrian Ioan <adrian.ioan@conkergroup.com>
2021-05-14 14:30:31 +0100

    Gets rid of _unprofessional_ strings

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,10 @@ exampleTree :: BST
 exampleTree = emptyBST
 
 main :: IO ()
-main = do 
-    let tree1 = BST.insert exampleTree (5, "pula") 
-        tree2 = BST.insert tree1 (3, "mea")
-        tree3 = BST.insert tree2 (-1, "ce")  
+main = do
+    let tree1 = BST.insert exampleTree (5, "hi")
+        tree2 = BST.insert tree1 (3, "I'm")
+        tree3 = BST.insert tree2 (-1, "a BST")
         tree4 = BST.delete tree3 3 in
-        print (BST.lookup 3 tree4)
+        print (BST.displayInOrder tree4)
 
